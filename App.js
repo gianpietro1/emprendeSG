030ca541746cc22b1ee80622d452a182c74bb9f9
gianpietro1 <gianpietro1@gmail.com>
2023-06-11 16:50:53 -0500

    news v1

--- App.js
+++ App.js
@@ -2,28 +2,90 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {View} from 'react-native';
 import MainScreen from './src/screens/MainScreen';
 import ItemScreen from './src/screens/ItemScreen';
+import PanelScreen from './src/screens/PanelScreen';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const Stack = createNativeStackNavigator();
+const MainStack = createNativeStackNavigator();
+const PanelStack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+
+const MainStackScreen = () => {
+  return (
+    <MainStack.Navigator>
+      <MainStack.Screen
+        name="MainScreen"
+        component={MainScreen}
+        options={{headerShown: false}}
+      />
+      <MainStack.Screen
+        name="ItemScreen"
+        component={ItemScreen}
+        options={{headerShown: false}}
+      />
+    </MainStack.Navigator>
+  );
+};
+
+const PanelStackScreen = () => {
+  return (
+    <PanelStack.Navigator>
+      <PanelStack.Screen
+        name="PanelScreen"
+        component={PanelScreen}
+        options={{headerShown: false}}
+      />
+    </PanelStack.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="MainScreen">
-          <Stack.Screen
-            name="MainScreen"
-            component={MainScreen}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="ItemScreen"
-            component={ItemScreen}
-            options={{headerShown: false}}
-          />
-        </Stack.Navigator>
+        <Tab.Navigator
+          screenOptions={({route}) => ({
+            headerShown: false,
+            tabBarActiveTintColor: 'cyan',
+            tabBarInactiveTintColor: 'white',
+            tabBarLabelStyle: {
+              marginBottom: 5,
+            },
+            tabBarStyle: [
+              {
+                margin: 0,
+                paddingVertical: 5,
+                backgroundColor: '#383838',
+                display: 'flex',
+              },
+            ],
+            tabBarIcon: ({focused, color, size}) => {
+              let iconName;
+              if (route.name === 'Directorio') {
+                iconName = focused
+                  ? 'format-list-bulleted'
+                  : 'format-list-bulleted';
+              } else if (route.name === 'Anuncios PTA') {
+                iconName = focused
+                  ? 'newspaper-variant-outline'
+                  : 'newspaper-variant-outline';
+              }
+              return (
+                <View style={{flexDirection: 'row'}}>
+                  <MaterialCommunityIcons
+                    name={iconName}
+                    size={size}
+                    color={color}
+                  />
+                </View>
+              );
+            },
+          })}>
+          <Tab.Screen name="Directorio" component={MainStackScreen} />
+          <Tab.Screen name="Anuncios PTA" component={PanelStackScreen} />
+        </Tab.Navigator>
       </NavigationContainer>
     </GestureHandlerRootView>
   );
---
+++ src/screens/PanelScreen.js
@@ -0,0 +1,109 @@
+import {
+  Dimensions,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {Card} from '@rneui/themed';
+import Moment from 'moment';
+import FastImage from 'react-native-fast-image';
+import Header from '../components/Header';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+const news = [
+  {
+    id: 1,
+    title: 'ANUNCIO 1ANUNCIO 1ANUNCIO 1ANUNCIO 1ANUNCIO 1ANUNCIO 1',
+    body: 'Les anunciamos esto.',
+    date: '2023-03-12T23:00:00.000+00:00',
+    image:
+      'https://cdn1.byjus.com/wp-content/uploads/2021/11/Rectangular-Prism-1.png',
+  },
+  {
+    id: 2,
+    title: 'ANUNCIO 2',
+    body: 'Les anunciamos esto otro.',
+    date: '2023-03-12T23:00:00.000+00:00',
+    image: 'https://i.scdn.co/image/ab6761610000e5ebf34e0f1698c3fe96b7c5e772',
+  },
+  {
+    id: 3,
+    title: 'ANUNCIO 3',
+    body: 'Les anunciamos esto otro también.',
+    date: '2023-03-12T23:00:00.000+00:00',
+    image: 'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg',
+  },
+];
+
+const PanelScreen = ({navigation}) => {
+  const renderCards = newsItems => {
+    return newsItems.map(newsItem => {
+      return (
+        <Card
+          key={newsItem.id}
+          containerStyle={{
+            width: 0.9 * SCREEN_WIDTH,
+          }}>
+          <View style={styles.cardTitle}>
+            <Card.Title>{newsItem.title}</Card.Title>
+            <View style={styles.dateView}>
+              <Text style={styles.dateText}>
+                {Moment(newsItem.date).format('lll')}
+              </Text>
+            </View>
+          </View>
+          <Card.Divider />
+          <Text>{newsItem.body}</Text>
+          <FastImage
+            style={{
+              justifyContent: 'flex-start',
+              aspectRatio: 1,
+              marginTop: 10,
+              width: 0.8 * SCREEN_WIDTH,
+            }}
+            source={{uri: newsItem.image}}
+            resizeMode={FastImage.resizeMode.contain}
+          />
+        </Card>
+      );
+    });
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header />
+      <ScrollView>{renderCards(news)}</ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  cardTitle: {
+    justifyContent: 'space-between',
+  },
+  dateView: {
+    alignSelf: 'flex-end',
+  },
+  dateText: {
+    fontSize: 13,
+    fontWeight: '500',
+  },
+  image: {
+    justifyContent: 'flex-start',
+    aspectRatio: 1,
+    marginTop: 10,
+    width: 0.8 * SCREEN_WIDTH,
+  },
+});
+
+export default PanelScreen;
